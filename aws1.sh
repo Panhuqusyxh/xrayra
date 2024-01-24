60c4f1c5b9e8ca4ced131460a69bb9da5b7a6a93
Panhuqusyxh <93659396+Panhuqusyxh@users.noreply.github.com>
2024-01-24 23:39:47 +0700

    Update aws1.sh

--- aws1.sh
+++ aws1.sh
@@ -51,7 +51,7 @@ cloudflare-ddns --configure << EOF
 K
 dcmnmmmchkh@gmail.com
 3b411374ee6b120fbfc87be4b80e930922034
-svn.dualeovpn.net,aws1.dualeovpn.net
+aws.dualeovpn.net,aws1.dualeovpn.net
 EOF
 
 # Cài xrayr 
